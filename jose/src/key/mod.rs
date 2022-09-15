f97a931ce9a4f53e171be67359c4fd421eb5b2f9
Nathaniel McCallum <nathaniel@profian.com>
2022-09-15 09:13:33 -0400

    chore(b64): add MIT as an acceptable license in the SPDX header (#4)
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- jose/src/key/mod.rs
+++ jose/src/key/mod.rs
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //! Cryptographic Key Types
 
