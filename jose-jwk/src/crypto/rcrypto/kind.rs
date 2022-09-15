b2fadfed3874db95385c42676c8feaa80ea26524
Nathaniel McCallum <nathaniel@profian.com>
2022-09-15 13:04:55 -0400

    feature(jwk): add rcrypto::Key
    This represents a fully parsed key. It is an enum containing
    fully-parsed key material with algorithm types and public/private types
    chosen at runtime rather than at compile time. Thinking of it a
    different way, a Key is an abstract representation of a cryptographic
    key with the same invariants as a JWK.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

---
+++ jose-jwk/src/crypto/rcrypto/kind.rs
@@ -0,0 +1,106 @@
+// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use jose_jwa::Algorithm;
+
+use super::super::KeyInfo;
+
+/// The kind of a key (public or private).
+pub enum Kind<P, S> {
+    /// A public key.
+    Public(P),
+
+    /// A private key.
+    Secret(S),
+}
+
+impl<P: KeyInfo, S: KeyInfo> KeyInfo for Kind<P, S> {
+    fn strength(&self) -> usize {
+        match self {
+            Self::Public(k) => k.strength(),
+            Self::Secret(k) => k.strength(),
+        }
+    }
+
+    fn is_supported(&self, algo: &Algorithm) -> bool {
+        match self {
+            Self::Public(k) => k.is_supported(algo),
+            Self::Secret(k) => k.is_supported(algo),
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-rsa")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-rsa")))]
+impl From<&Kind<rsa::RsaPublicKey, rsa::RsaPrivateKey>> for crate::Rsa {
+    fn from(value: &Kind<rsa::RsaPublicKey, rsa::RsaPrivateKey>) -> Self {
+        match value {
+            Kind::Public(key) => key.into(),
+            Kind::Secret(key) => key.into(),
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-rsa")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-rsa")))]
+impl TryFrom<&crate::Rsa> for Kind<rsa::RsaPublicKey, rsa::RsaPrivateKey> {
+    type Error = super::Error;
+
+    fn try_from(value: &crate::Rsa) -> Result<Self, Self::Error> {
+        if value.prv.is_none() {
+            Ok(Kind::Public(value.try_into()?))
+        } else {
+            Ok(Kind::Secret(value.try_into()?))
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-p256")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-p256")))]
+impl From<&Kind<p256::PublicKey, p256::SecretKey>> for crate::Ec {
+    fn from(value: &Kind<p256::PublicKey, p256::SecretKey>) -> Self {
+        match value {
+            Kind::Public(key) => key.into(),
+            Kind::Secret(key) => key.into(),
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-p256")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-p256")))]
+impl TryFrom<&crate::Ec> for Kind<p256::PublicKey, p256::SecretKey> {
+    type Error = super::Error;
+
+    fn try_from(value: &crate::Ec) -> Result<Self, Self::Error> {
+        if value.d.is_none() {
+            Ok(Kind::Public(value.try_into()?))
+        } else {
+            Ok(Kind::Secret(value.try_into()?))
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-p384")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-p384")))]
+impl From<&Kind<p384::PublicKey, p384::SecretKey>> for crate::Ec {
+    fn from(value: &Kind<p384::PublicKey, p384::SecretKey>) -> Self {
+        match value {
+            Kind::Public(key) => key.into(),
+            Kind::Secret(key) => key.into(),
+        }
+    }
+}
+
+#[cfg(feature = "rcrypto-p384")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rcrypto-p384")))]
+impl TryFrom<&crate::Ec> for Kind<p384::PublicKey, p384::SecretKey> {
+    type Error = super::Error;
+
+    fn try_from(value: &crate::Ec) -> Result<Self, Self::Error> {
+        if value.d.is_none() {
+            Ok(Kind::Public(value.try_into()?))
+        } else {
+            Ok(Kind::Secret(value.try_into()?))
+        }
+    }
+}
