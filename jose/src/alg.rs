cab6256d509db1f3e7ba2f1c0f1e89faa8f8398f
Tony Arcieri <bascule@gmail.com>
2023-05-20 18:39:24 -0600

    Remove `jose` crate
    We don't control this crate name and can't actually use it.
    
    It was here for a refactoring reference, but most of it has been moved
    into other crates already.
    
    Anyone wanting to further factor out any parts of it can find it in the
    commit history.

--- jose/src/alg.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-
-//! IANA-Defined Algorithms
-
-pub use jose_jwa::{Algorithm, Signing};
